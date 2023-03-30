85f1ac4512499146573358d15d41708457a44bab
Jack Puryear <ninjack.pokemon@gmail.com>
2023-03-30 07:24:40 -0400

    basic app now functioning!

--- main.py
+++ main.py
@@ -1,38 +1,94 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import (
+    Flask,
+    request,
+    render_template,
+    redirect,
+    url_for,
+    send_from_directory,
+    send_file,
+)
 from flask_sqlalchemy import SQLAlchemy
+import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///images.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///images.db"
+app.config["UPLOADED_IMAGES_DEST"] = "uploads/images"
+app.config["UPLOADED_IMAGES_URL"] = "uploads/images/"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255), nullable=False)
+    file_path = db.Column(db.String(255), nullable=False, unique=True)
     description = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
-        return f'<Image {self.filename}>'
+        return f"<Image {self.filename}>"
 
-@app.route('/')
+
+@app.route("/")
 def index():
     images = Image.query.all()
-    return render_template('index.html', images=images)
+    return render_template("index.html", images=images)
 
-@app.route('/add', methods=['POST'])
+
+@app.route("/add_image", methods=["POST"])
 def add_image():
-    filename = request.form['filename']
-    description = request.form['description']
-    new_image = Image(filename=filename, description=description)
-    db.session.add(new_image)
-    db.session.commit()
-    return redirect(url_for('index'))
+    if request.method == "POST":
+        file = request.files["file"]
+        description = request.form["description"]
 
-@app.route('/search', methods=['GET'])
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            file.save(file_path)
+
+            new_image = Image(file_path=filename, description=description)
+            db.session.add(new_image)
+            db.session.commit()
+
+            return redirect(url_for("index"))
+
+    return render_template("index.html")
+
+
+@app.route("/uploads/<path:filename>")
+def serve_uploaded_file(filename):
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+    return send_file(file_path, as_attachment=True)
+
+
+@app.route("/search", methods=["GET"])
 def search_image():
-    keyword = request.args.get('keyword')
+    keyword = request.args.get("keyword")
     images = Image.query.filter(Image.description.contains(keyword)).all()
-    return render_template('search_results.html', images=images)
+    return render_template("search_results.html", images=images)
 
-if __name__ == '__main__':
-    db.create_all()
+
+@app.route("/delete/<int:image_id>", methods=["POST"])
+def delete_image(image_id):
+    image = Image.query.get_or_404(image_id)
+    db.session.delete(image)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/uploads/images/<filename>")
+def uploaded_file(filename):
+    return send_from_directory(app.config["UPLOADED_IMAGES_DEST"], filename)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
